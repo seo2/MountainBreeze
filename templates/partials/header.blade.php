60bf90600c3154d1e1634f59a5f148922c382f67
Cristian <seo2@seo2.cl>
2021-02-16 20:08:56 -0300

    favicon

--- templates/partials/header.blade.php
+++ templates/partials/header.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="{{ get_bloginfo( 'charset' ) }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/svg+xml" href="@php bloginfo('template_url') @endphp/dist/img/isotipo.svg">
     @php(wp_head())
 </head>
 <body @php(body_class())>
