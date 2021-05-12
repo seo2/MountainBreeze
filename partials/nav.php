460ea3ab23af9ffa8a67159a062b20707f94dc1e
Cristian <seo2@seo2.cl>
2021-05-12 08:24:20 -0400

    avances nav

--- partials/nav.php
+++ partials/nav.php
@@ -90,12 +90,14 @@
   <div class="fixed top-20 pt-2 lg:pt-0 lg:top-20 z-40 w-full text-gris6 bg-blanco shadow">
   <div class="flex container max-w-screen-xl mx-auto items-center justify-between flex-row lg:px-24">
     <nav class="flex-grow md:pb-0 flex justify-between lg:justify-start flex-row uppercase">
-      <a class="px-4 lg:px-12 py-4 lg:mt-2 text-gris text-sm font-sans bg-transparent rounded-none md:mt-0 md:mr-1 hover:text-naranjo focus:text-naranjo focus:bg-gray-200 focus:outline-none focus:shadow-outline transition duration-200" href="<?php bloginfo('url'); ?>/">Mi inicio</a>
-      <a class="px-4 lg:px-12 py-4 lg:mt-2 text-gris text-sm font-sans bg-transparent rounded-none md:mt-0 md:mr-1 hover:text-naranjo focus:text-naranjo focus:bg-gray-200 focus:outline-none focus:shadow-outline transition duration-200" href="<?php bloginfo('url'); ?>/mis-talleres">Mis talleres</a>
-      <a class="px-4 lg:px-12 py-4 lg:mt-2 text-gris text-sm font-sans bg-transparent rounded-none md:mt-0 md:mr-1 hover:text-naranjo focus:text-naranjo focus:bg-gray-200 focus:outline-none focus:shadow-outline transition duration-200" href="<?php bloginfo('url'); ?>/como-funciona">Favoritos</a>
+      <a class="px-4 lg:px-8 py-4 lg:mt-2 text-gris text-sm font-sans bg-transparent rounded-none md:mt-0 md:mr-1 hover:text-naranjo focus:text-naranjo focus:bg-gray-200 focus:outline-none focus:shadow-outline transition duration-200" href="<?php bloginfo('url'); ?>/">Mi inicio</a>
+      <a class="px-4 lg:px-8 py-4 lg:mt-2 text-gris text-sm font-sans bg-transparent rounded-none md:mt-0 md:mr-1 hover:text-naranjo focus:text-naranjo focus:bg-gray-200 focus:outline-none focus:shadow-outline transition duration-200" href="<?php bloginfo('url'); ?>/mis-talleres">Mis talleres</a>
+      <a class="px-4 lg:px-8 py-4 lg:mt-2 text-gris text-sm font-sans bg-transparent rounded-none md:mt-0 md:mr-1 hover:text-naranjo focus:text-naranjo focus:bg-gray-200 focus:outline-none focus:shadow-outline transition duration-200" href="<?php bloginfo('url'); ?>/mis-talleres">Mis proyectos</a>
+      <a class="px-4 lg:px-8 py-4 lg:mt-2 text-gris text-sm font-sans bg-transparent rounded-none md:mt-0 md:mr-1 hover:text-naranjo focus:text-naranjo focus:bg-gray-200 focus:outline-none focus:shadow-outline transition duration-200" href="<?php bloginfo('url'); ?>/como-funciona">Favoritos</a>
+      <a class="px-4 lg:px-8 py-4 lg:mt-2 text-gris text-sm font-sans bg-transparent rounded-none md:mt-0 md:mr-1 hover:text-naranjo focus:text-naranjo focus:bg-gray-200 focus:outline-none focus:shadow-outline transition duration-200" href="<?php bloginfo('url'); ?>/mis-talleres">Mensajes</a>
     </nav>
     <nav class="flex-col flex-grow hidden md:flex md:justify-end md:flex-row">
-      <a class="px-4 lg:px-12 py-4 lg:mt-2 text-gris text-sm font-sans bg-transparent rounded-none md:mt-0 md:mr-1 hover:text-naranjo focus:text-naranjo focus:bg-gray-200 focus:outline-none focus:shadow-outline transition duration-200" href="<?php echo wp_logout_url( home_url() ); ?>">Cerrar Sesión <i class="fal fa-power-off ml-1"></i></a>
+      <a class="px-4 lg:px-8 py-4 lg:mt-2 text-gris text-sm font-sans bg-transparent rounded-none md:mt-0 md:mr-1 hover:text-naranjo focus:text-naranjo focus:bg-gray-200 focus:outline-none focus:shadow-outline transition duration-200" href="<?php echo wp_logout_url( home_url() ); ?>">Cerrar Sesión <i class="fal fa-power-off ml-1"></i></a>
     </nav>
   </div>
 </div>
--- templates/partials/nav-user.blade.php
+++ templates/partials/nav-user.blade.php
@@ -5,9 +5,12 @@
     <nav class="flex-grow md:pb-0 flex justify-between lg:justify-start flex-row uppercase">
       <a class="px-4 lg:px-12 py-4 lg:mt-2 text-gris text-sm font-sans bg-transparent rounded-none md:mt-0 md:mr-1 hover:text-naranjo focus:text-naranjo focus:bg-gray-200 focus:outline-none focus:shadow-outline transition duration-200" href="@php bloginfo('url'); @endphp/">Mi inicio</a>
       <a class="px-4 lg:px-12 py-4 lg:mt-2 text-gris text-sm font-sans bg-transparent rounded-none md:mt-0 md:mr-1 hover:text-naranjo focus:text-naranjo focus:bg-gray-200 focus:outline-none focus:shadow-outline transition duration-200" href="@php bloginfo('url'); @endphp/mis-talleres">Mis cursos</a>
+      <a class="px-4 lg:px-12 py-4 lg:mt-2 text-gris text-sm font-sans bg-transparent rounded-none md:mt-0 md:mr-1 hover:text-naranjo focus:text-naranjo focus:bg-gray-200 focus:outline-none focus:shadow-outline transition duration-200" href="@php bloginfo('url'); @endphp/como-funciona">Mis Proyectos</a>
       <a class="px-4 lg:px-12 py-4 lg:mt-2 text-gris text-sm font-sans bg-transparent rounded-none md:mt-0 md:mr-1 hover:text-naranjo focus:text-naranjo focus:bg-gray-200 focus:outline-none focus:shadow-outline transition duration-200" href="@php bloginfo('url'); @endphp/como-funciona">Favoritos</a>
+      <a class="px-4 lg:px-12 py-4 lg:mt-2 text-gris text-sm font-sans bg-transparent rounded-none md:mt-0 md:mr-1 hover:text-naranjo focus:text-naranjo focus:bg-gray-200 focus:outline-none focus:shadow-outline transition duration-200" href="@php bloginfo('url'); @endphp/como-funciona">Mensajes</a>
     </nav>
     <nav class="flex-col flex-grow hidden md:flex md:justify-end md:flex-row">
+      <a class="px-4 lg:px-12 py-4 lg:mt-2 text-gris text-sm font-sans bg-transparent rounded-none md:mt-0 md:mr-1 hover:text-naranjo focus:text-naranjo focus:bg-gray-200 focus:outline-none focus:shadow-outline transition duration-200" href="@php echo wp_logout_url( home_url() ); @endphp" >Editar Perfil <i class="fal fa-power-off ml-1"></i></a>
       <a class="px-4 lg:px-12 py-4 lg:mt-2 text-gris text-sm font-sans bg-transparent rounded-none md:mt-0 md:mr-1 hover:text-naranjo focus:text-naranjo focus:bg-gray-200 focus:outline-none focus:shadow-outline transition duration-200" href="@php echo wp_logout_url( home_url() ); @endphp" >Cerrar Sesión <i class="fal fa-power-off ml-1"></i></a>
     </nav>
 
