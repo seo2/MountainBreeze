d86d1698b64c72fbdaae8bafdbb9444c8422ab47
Cristian <seo2@seo2.cl>
2021-09-29 06:06:37 -0300

    .

---
+++ compiled/3bf5d7ba42bc09246da5894a85d9ed87666979c3.php
@@ -0,0 +1,85 @@
+<?php
+/*
+
+Template name: Proyectos
+
+*/
+?>
+
+
+
+<?php $__env->startSection('content'); ?> 
+
+<?php echo $__env->make('partials.nav-curso', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+<section class="mt-12">
+    <div class="flex container flex-col md:flex-row max-w-screen-xl mx-auto lg:items-center justify-left md:px-6 lg:px-12">
+        <div class="w-2/3 px-4">
+            <div>
+                <img src="<?php bloginfo('template_url') ?>/dist/img/foto_curso.jpg" alt="Curso" class="w-full">
+            </div>
+            <a  class="text-naranjo my-4 inline-block uppercase" href="#">loquemaspuedo</a>
+            <h1 class="text-negro mb-4 text-4xl" >Técnicas de sustentabilidad</h1>
+            <p  class="text-negro mb-12" >Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum.</p>
+            <hr>
+            <div class="grid lg:grid-cols-2 lg:gap-x-16 w-5/6 mx-auto lg:w-full leading-loose mt-12" x-data="{
+                valoraciones: [
+                    {
+                        unidad: 'plants',
+                        question: 'Et has minim alitr',
+                        answer: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores iure quas laudantium dicta impedit, est id delectus molestiae deleniti enim nobis rem et nihil.',
+                        isOpen: false,
+                    },
+                    {
+                        unidad: 'Unidad 2',
+                        question: 'Lorem ipsum dolor sit amet',
+                        answer: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi cumque, nulla harum aspernatur veniam ullam provident neque temporibus autem itaque odit.',
+                        isOpen: false,
+                    },
+                    {
+                        unidad: 'Unidad 3',
+                        question: 'mea aeterno eleifend antiopam?',
+                        answer: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi cumque, nulla harum aspernatur veniam ullam provident neque temporibus autem itaque odit.',
+                        isOpen: false,
+                    },
+                    {
+                        unidad: 'Unidad 2',
+                        question: 'Lorem ipsum dolor sit amet',
+                        answer: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi cumque, nulla harum aspernatur veniam ullam provident neque temporibus autem itaque odit.',
+                        isOpen: false,
+                    },
+                    {
+                        unidad: 'Unidad 3',
+                        question: 'mea aeterno eleifend antiopam?',
+                        answer: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi cumque, nulla harum aspernatur veniam ullam provident neque temporibus autem itaque odit.',
+                        isOpen: false,
+                    }
+                ]
+            }">
+                <template x-for="valoracion in valoraciones" :key="valoracion.question">
+                    <div class="mb-16">
+                        <img src="https://source.unsplash.com/500x500/?plants" alt="">
+                        <span class="text-naranjo uppercase text-sm">Nombre usuario</span>
+                        <h3 class="text-negro uppercase font-festivo6 text-2xl leading-tight">Nombre del proyecto que hizo el usuario</h3>
+                    </div>
+                </template>
+
+            </div>
+        </div>
+        <div class="w-1/3 px-4">
+            <?php echo $__env->make('partials.sidebar-curso', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+        </div>
+    </div>
+</section>
+
+<?php echo $__env->make('partials.interesar', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+
+
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('footer'); ?>
+
+
+<?php $__env->stopSection(); ?>  
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Applications/MAMP/htdocs/herenciacolectiva/wp-content/themes/mountainbreeze/templates/page-proyectos.blade.php ENDPATH**/ ?>
---
+++ compiled/c3215de9ec8e7261048efbc4a874807ce6af9402.php
@@ -0,0 +1,15 @@
+
+<section class="bg-blanco mt-36 border-b border-gris5 border-solid h-16 ">
+    <div class="flex container max-w-screen-xl mx-auto items-center justify-between flex-row lg:px-24">
+        <nav class="flex-grow md:pb-0 flex justify-between lg:justify-start flex-row uppercase">
+          <a class="px-4 lg:px-12 py-4 text-gris text-sm font-sans bg-transparent rounded-none md:mt-0 md:mr-1 hover:text-naranjo focus:text-naranjo focus:bg-gray-200 focus:outline-none focus:shadow-outline transition duration-200" href="/curso/">Intro</a>
+          <a class="px-4 lg:px-12 py-4 text-gris text-sm font-sans bg-transparent rounded-none md:mt-0 md:mr-1 hover:text-naranjo focus:text-naranjo focus:bg-gray-200 focus:outline-none focus:shadow-outline transition duration-200" href="/unidades/">Unidades</a>
+          <a class="px-4 lg:px-12 py-4 text-gris text-sm font-sans bg-transparent rounded-none md:mt-0 md:mr-1 hover:text-naranjo focus:text-naranjo focus:bg-gray-200 focus:outline-none focus:shadow-outline transition duration-200" href="/valoraciones/">Valoración</a>
+          <a class="px-4 lg:px-12 py-4 text-gris text-sm font-sans bg-transparent rounded-none md:mt-0 md:mr-1 hover:text-naranjo focus:text-naranjo focus:bg-gray-200 focus:outline-none focus:shadow-outline transition duration-200" href="/proyectos/">Proyectos</a>
+        </nav>
+        <nav class="flex-col flex-grow hidden md:flex md:justify-end md:flex-row">
+          <a class="px-4 lg:px-6 lg:mr-12 py-4 text-gris bg-gris6 text-xl font-sans rounded-none hover:text-beige hover:bg-negro focus:text-naranjo focus:bg-gray-200 focus:outline-none focus:shadow-outline transition duration-200" href="#"><i class="fal fa-heart"></i></a>
+        </nav>
+    </div>
+</section>
+<?php /**PATH /Applications/MAMP/htdocs/herenciacolectiva/wp-content/themes/mountainbreeze/templates/partials/nav-curso.blade.php ENDPATH**/ ?>
