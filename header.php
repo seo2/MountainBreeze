1d4f39a88f7be47373d316cb25cae715451d7b45
Cristian <seo2@seo2.cl>
2021-10-13 11:53:20 -0300

    Update header.php

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/svg+xml" href="<?php bloginfo('template_url') ?>/dist/img/isotipo.svg">
     <?php wp_head(); ?>
-    <script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/665255aeb907e62c6320dfe41/1853b43d5c7ace6beb068afcd.js");</script>
+    <script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/665255aeb907e62c6320dfe41/41c9332e97c61342682389440.js");</script>
 </head>
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
