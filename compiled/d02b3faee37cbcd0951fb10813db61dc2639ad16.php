e93619636a99b11179cd7bb96e55dd345b9b49e9
Cristian <seo2@seo2.cl>
2021-10-14 10:08:46 -0300

    avances perfil

--- compiled/d02b3faee37cbcd0951fb10813db61dc2639ad16.php
+++ compiled/d02b3faee37cbcd0951fb10813db61dc2639ad16.php
@@ -16,15 +16,34 @@
                 <p class="text-beige mb-4">Suscríbete al newsletter de Herencia Colectiva y recibe noticias sobre los talleres y la comunidad.</p>
                 <h3 class="text-beige font-bold">¡Súmate!</h3>
             </div>
-            <form class="col-start-1 col-span-12 lg:col-start-7 lg:col-span-4" action="https://herenciacolectiva.us1.list-manage.com/subscribe/post?u=665255aeb907e62c6320dfe41&amp;id=a6b5a8b004" method="post" name="mc-embedded-subscribe-form"  target="_blank" novalidate>
-                <div class="bg-blanco grid grid-cols-12 gap2">
-                    <span class="col-span-1 text-center py-2 text-gris4"><i class="fal fa-envelope"></i></span>
-                    <input type="email" name="EMAIL" class="border-none w-full rounded-none px-3 py-2 col-start-2 col-span-11 placeholder-gris4" placeholder="Escribe tu email"/>
-                    <input type="hidden" name="TAGS" value="formulario-sitio"/>
-                </div>
-                <input type="hidden" name="b_88d81859b6f9ce82934e29480_801c8ebe9c" tabindex="-1" value="">
-                <button type="submit" name="subscribe" class="bg-rosado text-white px-3 py-2 rounded-none w-full mt-4 uppercase hover:bg-negro transition duration-200">Suscribirme</button>
-            </form>
+            
+            
+
+            <div class="col-start-1 col-span-12 lg:col-start-7 lg:col-span-4" >
+                <!-- Begin Mailchimp Signup Form -->
+                <div id="mc_embed_signup">
+                    <form action="https://herenciacolectiva.us1.list-manage.com/subscribe/post?u=665255aeb907e62c6320dfe41&amp;id=a6b5a8b004" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                        <div id="mc_embed_signup_scroll">
+                        
+                            <div class="mc-field-group bg-blanco grid grid-cols-12 gap2">
+                                <span class="col-span-1 text-center py-2 text-gris4"><i class="fal fa-envelope"></i></span>
+                                <input type="email" value="" name="EMAIL" class="border-none w-full rounded-none px-3 py-2 col-start-2 col-span-11 placeholder-gris4" placeholder="Escribe tu email" id="mce-EMAIL">
+                            </div>
+                            <div id="mce-responses" class="clear">
+                                <div class="response" id="mce-error-response" style="display:none"></div>
+                                <div class="response" id="mce-success-response" style="display:none"></div>
+                            </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_665255aeb907e62c6320dfe41_a6b5a8b004" tabindex="-1" value=""></div>
+                        <div class="clear"><input type="submit" value="Subscribe" name="Suscribirme" id="mc-embedded-subscribe" class="bg-rosado text-white px-3 py-2 rounded-none w-full mt-4 uppercase hover:bg-negro transition duration-200"></div>
+                        </div>
+                    </form>
+                    </div>
+                    
+                    <!--End mc_embed_signup-->
+            </div>
+
+
+
         </div>
     </div>
 </div><?php /**PATH /Applications/MAMP/htdocs/herenciacolectiva/wp-content/themes/mountainbreeze/templates/partials/suscribirse.blade.php ENDPATH**/ ?>
