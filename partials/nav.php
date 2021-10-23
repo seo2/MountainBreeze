596ac85eb383fa15d6f561d0ee976457f3656e03
Cristian <seo2@seo2.cl>
2021-10-23 10:36:50 -0300

    Update nav.php

--- partials/nav.php
+++ partials/nav.php
@@ -83,7 +83,7 @@
         <span class="text-beige bg-naranjo absolute h-4 w-4 text-xs top-1 right-1 text-center leading-4 rounded-full"><?php echo WC()->cart->get_cart_contents_count(); ?></span>
       </a>
       <!-- span for currency selector -->
-      <span class="px-4 py-2 mt-2 text-gris6 text-sm md:mt-0 md:ml-2 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline transition duration-200 relative flex" id="currencySelector">
+      <span class="px-4  id="currencySelector">
         
         <?php
         // use wordpress shortcode to display currency selector
