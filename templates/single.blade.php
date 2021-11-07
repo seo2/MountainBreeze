d0cc21f03e4d46d74031dde8e17620f906d61b0f
Cristian <seo2@seo2.cl>
2021-11-07 00:08:56 -0300

    Update single.blade.php

--- templates/single.blade.php
+++ templates/single.blade.php
@@ -11,9 +11,8 @@ $imagen_banner_taller = get_field('imagen_banner_taller');
 
 <section class="w-full flex pt-6 pb-6 mt-32 h-48 lg:bg-cover bg-left-bottom lg:bg-bottom bg-no-repeat bg-azul relative" @if ($imagen_banner_taller)  style="background-image: url({{$imagen_banner_taller}});" @endif >
     @if ($imagen_banner_taller)  
-        
+        <div class="absolute inset-0 w-full h-full  bg-gradient-to-t from-black opacity-50"></div>
     @endif 
-    <div class="absolute inset-0 w-full h-full  bg-gradient-to-t from-black opacity-50"></div>
     <div class="container flex flex-row h-100 max-w-screen-xl mx-auto justify-between lg:px-32 relative z-10">
         <div class="relative w-2/3">
             <a href="/mis-talleres/" class="text-blanco uppercase relative top-2 hover:text-naranjo transition duration-200"><i class="fak fa-back mr-4"></i> Volver</a>
