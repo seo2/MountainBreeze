9a793cf5e9a156b4e4c9f8a019122bfdd57b8eaa
Cristian <seo2@seo2.cl>
2021-11-25 16:18:24 -0300

    Update single.blade.php

--- templates/single.blade.php
+++ templates/single.blade.php
@@ -10,26 +10,25 @@
     $post_id = get_the_ID();
 @endphp
 
-<section class="w-full flex pt-6 pb-6 mt-32 h-48 lg:bg-cover bg-left-bottom lg:bg-bottom bg-no-repeat bg-azul relative" @if ($imagen_banner_taller)  style="background-image: url({{$imagen_banner_taller}});" @endif >
+<section class="w-full flex py-4 md:py-6 mt-32 md:h-48 lg:bg-cover bg-left-bottom lg:bg-bottom bg-no-repeat bg-azul relative" @if ($imagen_banner_taller)  style="background-image: url({{$imagen_banner_taller}});" @endif >
     @if ($imagen_banner_taller)  
         <div class="absolute inset-0 w-full h-full  bg-gradient-to-t from-black opacity-50"></div>
     @endif 
-    <div class="container flex flex-row h-100 max-w-screen-xl mx-auto justify-between px-6 lg:px-32 relative z-10">
-        <div class="relative w-2/3">
-            <a href="/mis-talleres/" class="text-blanco uppercase relative top-2 hover:text-naranjo transition duration-200"><i class="fak fa-back mr-4"></i> Volver</a>
-            <h1 class="text-beige font-festivo6 text-2xl lg:text-4xl absolute bottom-0">{{ the_title() }}</h1>
+    <div class="container flex flex-col md:flex-row h-100 max-w-screen-xl mx-auto justify-between px-6 lg:px-32 relative z-10">
+        <div class="relative w-full md:w-2/3 mb-4 md:mb-0">
+            <a href="/mis-talleres/" class="text-blanco uppercase relative top-2 hover:text-naranjo transition duration-200 block"><i class="fak fa-back mr-4"></i> Volver</a>
+            <h1 class="text-beige font-festivo6 text-2xl lg:text-4xl absolute bottom-0 hidden md:block">{{ the_title() }}</h1>
         </div> 
-        <div class="w-1/3 flex justify-center content-center items-center">
+        <div class="w-full md:w-1/3 flex md:justify-center content-center items-center">
             @php
                 $permalink  = get_permalink( get_field('tallerista'));
                 $title      = get_the_title( get_field('tallerista') );
                 $url        = get_the_post_thumbnail_url( get_field('tallerista')  );
                 $instagram  = get_field( 'instagram', get_field('tallerista') );
             @endphp
-            <div class="rounded-full h-20 w-20 flex items-center justify-center bg-naranjo border border-negro mr-4 bg-cover" style="background-image: url('<?php echo $url; ?>">
-
-            </div>
+            <div class="rounded-full h-20 w-20 flex items-center justify-center bg-naranjo border border-negro mr-4 bg-cover" style="background-image: url('<?php echo $url; ?>"></div>
             <div>
+                <h1 class="text-beige font-festivo6 text-2xl lg:text-4xl md:hidden ">{{ the_title() }}</h1>
                 <a href="@php echo $permalink; @endphp" class="text-beige uppercase text-xle hover:underline">@php echo $title; @endphp</p>
                 <a href="https://instagram.com/@php echo $instagram; @endphp" class="text-beige hover:underline">@php echo '@'.$instagram; @endphp</a>
             </div>
@@ -37,7 +36,7 @@
     </div>
 </section>
 
-<section class="my-12" id="taller">
+<section class="my-2 md:my-12" id="taller">
     <div class="flex container max-w-screen-xl mx-auto justify-between flex-col lg:flex-row px-6 lg:px-32 gap-12">
         <div class="w-full md:w-2/3">
             @php
