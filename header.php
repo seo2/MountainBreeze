cfa21a34ac727d42474e5559448c5fc7fc3c5c3b
Cristian <seo2@seo2.cl>
2022-02-28 09:10:07 -0300

    Update header.php

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="<?php echo get_bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/svg+xml" href="<?php bloginfo('template_url') ?>/dist/img/isotipo.svg">
+    
     <?php wp_head(); ?>
     <script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/665255aeb907e62c6320dfe41/41c9332e97c61342682389440.js");</script>
     
